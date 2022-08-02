c5d6c8e416407b305cffa2decd7b05e9048287ee
Luis Alfonso Gomez Tinoco <luchoagomezt@gmail.com>
2022-08-02 06:16:00 -0400

    Adding pytest report html and coverage

--- utest/test_forecaster.py
+++ utest/test_forecaster.py
@@ -37,8 +37,23 @@ def up_rain_forecaster():
 
     return Forecaster(MockOpenWeather())
 
+
 @pytest.fixture()
-def up_clear_forecaster():
+def down_snow_forecaster():
+    from forecaster import Forecaster
+
+    class MockOpenWeather:
+        def get_current_weather_and_temperature(self) -> (str, float):
+            return "Snow", 288.91
+
+        def get_forecast_temperature(self) -> float:
+            return 286.91
+
+    return Forecaster(MockOpenWeather())
+
+
+@pytest.fixture()
+def down_clear_forecaster():
     from forecaster import Forecaster
 
     class MockOpenWeather:
@@ -46,26 +61,36 @@ def up_clear_forecaster():
             return "Clear", 288.91
 
         def get_forecast_temperature(self) -> float:
-            return 290.91
+            return 286.91
 
     return Forecaster(MockOpenWeather())
 
 
 def test_print_with_api(forecaster):
-    print(forecaster)
+    rel = forecaster.calculate_temperature_tendency_and_weather()
+    assert rel[0] in ["SAME", "UP", "DOWN"]
+    assert rel[1] in ["Steady green", "Steady red", "Flashing red", "Flashing white"]
 
 
-def test_print_with_mock_clouds(same_clouds_forecaster):
-    print(same_clouds_forecaster)
+def test_up_with_clouds(same_clouds_forecaster):
+    rel = same_clouds_forecaster.calculate_temperature_tendency_and_weather()
+    assert rel[0] == "SAME"
+    assert rel[1] == "Steady red"
 
 
-def test_temperature_tendency_and_weather_with_rain(up_rain_forecaster):
+def test_up_and_flashing_red(up_rain_forecaster):
     rel = up_rain_forecaster.calculate_temperature_tendency_and_weather()
     assert rel[0] == "UP"
     assert rel[1] == "Flashing red"
 
 
-def test_print_with_mock_rain(up_clear_forecaster):
-    rel = up_clear_forecaster.calculate_temperature_tendency_and_weather()
-    assert rel[0] == "UP"
+def test_down_and_clear(down_clear_forecaster):
+    rel = down_clear_forecaster.calculate_temperature_tendency_and_weather()
+    assert rel[0] == "DOWN"
     assert rel[1] == "Steady green"
+
+
+def test_down_and_flashing_white(down_snow_forecaster):
+    rel = down_snow_forecaster.calculate_temperature_tendency_and_weather()
+    assert rel[0] == "DOWN"
+    assert rel[1] == "Flashing white"
